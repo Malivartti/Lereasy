a92be9958dbe61f21bf50c6990eddc2ac8570009
Ilya <wickedbun@yandex.ru>
2021-04-24 16:01:11 +0500

    part 2

--- data/users.py
+++ data/users.py
@@ -1,8 +1,7 @@
-import datetime
 import sqlalchemy
-from sqlalchemy import orm
 from werkzeug.security import check_password_hash, generate_password_hash
 from .db_session import SqlAlchemyBase
+
 
 
 class User(SqlAlchemyBase):
@@ -10,10 +9,9 @@ class User(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_login import login_required, LoginManager, login_user, logout_user
+from werkzeug.security import check_password_hash
+
 from data import db_session
 from data.topics import Topic
 from data.users import User
 from werkzeug.utils import redirect
-from forms.user import RegisterForm
+from forms.user import RegisterForm, AuthorizationForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+manager = LoginManager(app)
 
 
+# <------------------------------------------------------------>
+@manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+# <------------------------------------------------------------>
+
+@app.route("/")
 @app.route("/home")
 def index():
     return render_template("home.html")
@@ -39,10 +52,33 @@ def reqister():
     return render_template('register.html', title='Регистрация', form=form)
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("base.html")
+    form = AuthorizationForm()
+    db_session.global_init("db/blogs.db")
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if check_password_hash(user.hashed_password, form.password.data):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page)
 
+        return render_template('login.html', title='Авторизация', form=form,
+                               message="Неправильно введен логин или пароль")
+
+    return render_template("login.html", title='Авторизация', form=form)
+
+
+# <------------------------------------------------------------>
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return "Hello"
+
+
+# <------------------------------------------------------------>
 
 @app.route("/topics")
 def topics():
@@ -50,6 +86,44 @@ def topics():
     db_sess = db_session.create_session()
     it = db_sess.query(Topic).all()
     return render_template("topics.html", form=it)
+
+
+@app.route("/topics/<int:id>")
+def particular_topic(id):
+    return render_template("topic_block.html")
+
+
+@app.route("/topics/id/material")
+def material():
+    return render_template("topic_block_material.html")
+
+
+@app.route("/topics/id/practice")
+def practice():
+    return render_template("base.html")
+
+
+# ----------------------- Видно администратору ----------------------
+@app.route("/topics/<int:id>/ADmaterial", methods=['POST', 'GET'])
+def ADmaterial(id):
+    if request.method == 'GET':
+        return render_template("topic_block_material.html")
+    elif request.method == 'POST':
+        print(request.form['name'])
+        print(request.form['added_by'])
+        print(request.form['img'])
+        print(request.form['comment'])
+        return 'True'
+
+
+@app.route("/topics/<int:id>/ADpractice")
+def ADpractice(id):
+    return render_template("base.html")
+
+
+@app.route("/topics/add")
+def add_topic():
+    pass
 
 
 if __name__ == '__main__':
